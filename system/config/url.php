105d4b0f83843af364bef28b926f7b95791a26b3
Nikolay <turov96@yandex.ru>
2017-04-26 13:17:47 +0300

    kohana update

--- system/config/url.php
+++ system/config/url.php
@@ -3,8 +3,16 @@
 return array(
 
 	'trusted_hosts' => array(
-		'localhost',
-		'prezentit'
+		// Set up your hostnames here
+		//
+		// Example:
+		//
+		//        'example\.org',
+		//        '.*\.example\.org',
+		//
+		// Do not forget to escape your dots (.) as these are regex patterns.
+		// These patterns should always fully match,
+		// as they are prepended with `^` and appended with `$`
 	),
 
 );
