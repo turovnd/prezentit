b6fb96fa1bf1bb82e602dd6b34348bba9c13fe80
Nikolay <turov96@yandex.ru>
2017-05-03 14:31:06 +0300

    section wrapper init

--- application/views/app/pages/presentation.php
+++ application/views/app/pages/presentation.php
@@ -1,4 +1,4 @@
-<div id="toggleInstruction" class="cursor-pointer" data-toggle="collapse" data-area="instruction" data-opened="false" onclick="document.getElementById('instruction').removeAttribute('data-height')">
+<div id="toggleInstruction" class="presentation__header-wrapper" data-toggle="collapse" data-area="instruction" data-opened="false" onclick="document.getElementById('instruction').removeAttribute('data-height')">
     <?= View::factory('app/blocks/header-presentation'); ?>
 </div>
 
@@ -14,9 +14,25 @@
 
 </aside>
 
+<div class="presentation__navigation-btns">
+    <a role="button" class="presentation__navigation-btn presentation__navigation-btn--left">
+        <i class="fa fa-angle-left" aria-hidden="true"></i>
+    </a>
+    <a role="button" class="presentation__navigation-btn presentation__navigation-btn--right">
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+    </a>
+</div>
+
+
 <div class="presentation__slides">
 
-    <section class="presentation__slide">
+    <section class="presentation__slide" style="background: red">
+        <?= View::factory('app/blocks/slide-presentation/heading'); ?>
+    </section>
+    <section class="presentation__slide" style="background: blue">
+        <?= View::factory('app/blocks/slide-presentation/heading'); ?>
+    </section>
+    <section class="presentation__slide" style="background: green">
         <?= View::factory('app/blocks/slide-presentation/heading'); ?>
     </section>
 
--- assets/static/js/presentation.js
+++ assets/static/js/presentation.js
@@ -1,12 +1,102 @@
-var present = function (present) {
+let present = function (present) {
 
-    var instruction     = null,
-        fullScreenEl    = null;
+    let instruction     = null,
+        fullScreenEl    = null,
+        slides          = null,
+        slidesHash      = null,
+        curSlide        = null,
+        nextSlideBtn    = null,
+        prevSlideBtn    = null;
 
 
-    var prepare_ = function () {
+    let prepare_ = function () {
         instruction     = document.getElementById('toggleInstruction');
         fullScreenEl    = document.getElementsByClassName('presentation')[0];
+        slides          = document.getElementsByClassName('presentation__slide');
+        slidesHash      = location.pathname;
+        nextSlideBtn    = document.getElementsByClassName('presentation__navigation-btn--right')[0];
+        prevSlideBtn    = document.getElementsByClassName('presentation__navigation-btn--left')[0];
+
+        if (nextSlideBtn) {
+            nextSlideBtn.addEventListener('click', toNextSlide);
+        }
+        if (prevSlideBtn) {
+            prevSlideBtn.addEventListener('click', toPrevSlide);
+        }
+        prepareSlides_(slides);
+
+        document.addEventListener("keydown", keyDownFunction, false);
+
+    };
+
+
+    /**
+     * Prepare Slides
+     * - select current slide from Cookie if exist
+     * @param slides
+     * @private
+     */
+    let prepareSlides_ = function (slides) {
+        if (pit.cookies.get('cur_slide') && pit.cookies.get('cur_slide').match(new RegExp(slidesHash))) {
+            curSlide = parseInt(pit.cookies.get('cur_slide').replace(location.pathname, ''));
+        } else {
+            curSlide = 0;
+        }
+
+        switchSlides()
+    };
+
+    /**
+     * Switch to Next Slide
+     * @private
+     */
+    let toNextSlide = function () {
+        if (curSlide < slides.length - 1) {
+            curSlide++;
+            switchSlides()
+        }
+    };
+
+    /**
+     * Switch to Previous Slide
+     * @private
+     */
+    let toPrevSlide = function () {
+        if (curSlide > 0) {
+            curSlide--;
+            switchSlides()
+        }
+    };
+
+
+    /**
+     * Switch slides
+     * - include updating current slide in Cookie
+     * @private
+     */
+    let switchSlides = function () {
+
+        pit.cookies.set({
+            name: 'cur_slide',
+            value: location.pathname + "~" + location.pathname + curSlide,
+            expires: 21600,
+            path: '/'
+        });
+
+        for (let i = 0; i < slides.length; i++) {
+            if (i < curSlide) {
+                slides[i].classList.remove('presentation__slide--active', 'presentation__slide--after');
+                slides[i].classList.add('presentation__slide--before');
+                continue;
+            }
+            if (i > curSlide) {
+                slides[i].classList.remove('presentation__slide--active', 'presentation__slide--before');
+                slides[i].classList.add('presentation__slide--after');
+            }
+        }
+
+        slides[curSlide].classList.remove('presentation__slide--after', 'presentation__slide--before');
+        slides[curSlide].classList.add('presentation__slide--active');
     };
 
 
@@ -15,7 +105,7 @@ var present = function (present) {
      */
     present.toggleFullScreen = function () {
         if (fullScreenEl) {
-            if( fullScreenEl.dataset.fullscreen == "true" ) {
+            if (fullScreenEl.dataset.fullscreen === "true") {
                 fullScreenEl.dataset.fullscreen = false;
                 cancelFullScreen()
             } else {
@@ -27,26 +117,34 @@ var present = function (present) {
         }
     };
 
-    var launchFullScreen = function (element) {
-        if(element.requestFullScreen) {
+    /**
+     * Launch Full Screen On Click
+     * @param element
+     * @private
+     */
+    let launchFullScreen = function (element) {
+        if (element.requestFullScreen) {
             element.requestFullScreen();
-        } else if(element.mozRequestFullScreen) {
+        } else if (element.mozRequestFullScreen) {
             element.mozRequestFullScreen();
-        } else if(element.webkitRequestFullScreen) {
+        } else if (element.webkitRequestFullScreen) {
             element.webkitRequestFullScreen();
         }
     };
 
-    var cancelFullScreen = function () {
-        if(document.cancelFullScreen) {
+    /**
+     * Close Full Screen On Click
+     * @private
+     */
+    let cancelFullScreen = function () {
+        if (document.cancelFullScreen) {
             document.cancelFullScreen();
-        } else if(document.mozCancelFullScreen) {
+        } else if (document.mozCancelFullScreen) {
             document.mozCancelFullScreen();
-        } else if(document.webkitCancelFullScreen) {
+        } else if (document.webkitCancelFullScreen) {
             document.webkitCancelFullScreen();
         }
     };
-
 
 
     /**
@@ -55,7 +153,7 @@ var present = function (present) {
     present.toggleInstruction = function () {
         if (instruction) {
 
-            if(instruction.dataset.opened == "false") {
+            if (instruction.dataset.opened === "false") {
                 instruction.classList.toggle('hide');
 
                 if (!instruction.classList.contains('hide')) {
@@ -71,6 +169,49 @@ var present = function (present) {
             console.log("Error: instruction doesn't exist");
         }
     };
+
+    /**
+     * Key Down Navigation
+     * @param e
+     */
+    let keyDownFunction = function (e) {
+        let keyCode = e.keyCode;
+
+        if (keyCode === 39 || keyCode === 32) {
+            toNextSlide();
+            return;
+        }
+        if (keyCode === 37) {
+            toPrevSlide();
+            return;
+        }
+        if (keyCode === 81) {
+            /**
+             * TODO: hide||show Keyboard shortcuts
+             */
+            return;
+        }
+        if (keyCode === 73) {
+            present.toggleInstruction();
+            return;
+        }
+        if (keyCode === 72) {
+            /**
+             * TODO: hide||show results
+             */
+            return;
+        }
+        if (keyCode === 70) {
+            present.toggleFullScreen();
+            return;
+        }
+        if (keyCode === 67) {
+            /**
+             * TODO: open||close voting
+             */
+        }
+    };
+
 
     present.init = function () {
         prepare_();
