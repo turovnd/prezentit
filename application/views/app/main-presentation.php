c4afca4251bf4cd62059ab771fd0bf9d1628f656
Nikolay <turov96@yandex.ru>
2017-06-01 02:15:31 +0300

    fixed

--- application/views/app/main-presentation.php
+++ application/views/app/main-presentation.php
@@ -12,8 +12,8 @@
 
     <link rel="stylesheet" href="<?=$assets; ?>vendor/font-awesome/css/font-awesome.css?v=<?= filemtime("assets/vendor/font-awesome/css/font-awesome.css") ?>">
 
-    <link rel="stylesheet" href="<?=$assets; ?>frontend/bundles/present.min.css">
-    <script type="text/javascript" src="<?=$assets; ?>frontend/bundles/present.min.js"></script>
+    <link rel="stylesheet" href="<?=$assets; ?>frontend/bundles/present.min.css?v=<?= filemtime("assets/frontend/bundles/present.min.css") ?>">
+    <script type="text/javascript" src="<?=$assets; ?>frontend/bundles/present.min.js?v=<?= filemtime("assets/frontend/bundles/present.min.js") ?>"></script>
 
     <script defer type="text/javascript">
         function ready() {
